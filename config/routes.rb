90981f12b4950fb71950dbc2fc7e3228bef61b6e
Kakimzhanova Kyz-Zhibek <z.kakimzhanova@mail.ru>
2018-07-14 23:34:15 +0600

    add pages controller and index.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'users/create'
   get 'users/edit'
   get 'users/destroy'
+  root 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
