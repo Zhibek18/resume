80146ac6919d8766e1008168682fd2794d4ca862
Kakimzhanova Kyz-Zhibek <z.kakimzhanova@mail.ru>
2018-07-15 22:03:13 +0600

    change redirect to show after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      redirect_to 'pages#index'
+      redirect_to @user
     else
       render :new
     end
